693bd4d7d2db2427fc0eb4c901425a1abfbeec90
jarparrish <jarparrish@gmail.com>
2020-10-07 19:24:38 -0700

    simple sliding window implemented

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -42,8 +42,8 @@ def eating_cookies(n):
 print(eating_cookies(10))
 
 
-""" if __name__ == "__main__":
+if __name__ == "__main__":
     # Use the main function here to test out your implementation
     num_cookies = 5
 
-    print(f"There are {eating_cookies(num_cookies)} ways for Cookie Monster to each {num_cookies} cookies") """
+    print(f"There are {eating_cookies(num_cookies)} ways for Cookie Monster to each {num_cookies} cookies")
--- sliding_window_max/sliding_window_max.py
+++ sliding_window_max/sliding_window_max.py
@@ -5,8 +5,23 @@ Returns: a List of integers
 def sliding_window_max(nums, k):
     # Your code here
 
-    pass
+    #make an array to hold maxes
+    max_list = []
 
+    # make nested for loops to check for max
+    for i in range(len(nums) - (k - 1)):
+        k_list = []
+        
+        for j in range(k):
+            k_list.append(nums[i + j])
+        max_list.append(max(k_list))
+
+    return max_list
+
+arr = [1, 3, -1, -3, 5, 3, 6, 7]
+k = 3
+
+print(sliding_window_max(arr, k))
 
 if __name__ == '__main__':
     # Use the main function here to test out your implementation 
